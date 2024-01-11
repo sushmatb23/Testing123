6d734b6d60ae81371678e55b16b25e36083bb5e7
Ankit <Ankit@DESKTOP-O4P8250>
2024-01-11 13:07:52 +0530

    by sushma

---
+++ Git_program/src/com/cjc/demo/demo1.java
@@ -0,0 +1,21 @@
+package com.cjc.demo;
+
+public class demo1 {
+
+	
+	public void m1()
+	{
+		System.out.println("this is m1 method");
+	}
+	
+	public void m2()
+	{
+		System.out.println("success");
+	}
+	
+		public static void main(String[] args) {
+		System.out.println("Hello");
+	
+	}
+	
+}
